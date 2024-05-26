6f3ee3b4e6f7cb34d008d5d66f110172f406cd99
Leonardo Borck <l.borck@edu.univali.br>
2024-05-25 23:22:23 -0300

    #Altera para nome ficar em ordem no excel

--- BinPackingProblemParallel/Program.cs
+++ BinPackingProblemParallel/Program.cs
@@ -10,16 +10,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] classes = { "CLASS01" };//, "CLASS02", "CLASS03", "CLASS04", "CLASS05", "CLASS06", "CLASS07", "CLASS08", "CLASS09", "CLASS10"};
-        string[] numeroDeItens = { "020" };//, "040", "060", "080", "100" };
+        string[] classes = { "CLASS01", "CLASS02", "CLASS03", "CLASS04", "CLASS05", "CLASS06", "CLASS07", "CLASS08", "CLASS09", "CLASS10"};
+        string[] numeroDeItens = { "020", "040", "060", "080", "100" };
         string[] instancias = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10" };
 
         string[] classesAleatorio = { "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10" };
-        string[] instanciasAleatorio = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50" };
-
+        string[] instanciasAleatorio = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50" };
+        string[] instanciasAleatorioNome = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50" };
         List<BaseDeDados> basesDeDados = new List<BaseDeDados>();
 
         bool tamanhoAleatorio = true;
+
         if (!tamanhoAleatorio)
             for (int classe = 0; classe < classes.Count(); classe++)
             {
@@ -49,7 +50,7 @@ internal class Program
                 {
                     var stream = new StreamReader($"..\\..\\..\\base\\MB\\MB_{classesAleatorio[classe]}_{instanciasAleatorio[instancia]}.json");
                     var baseDeDados = JsonConvert.DeserializeObject<BaseDeDados>(stream.ReadToEnd());
-                    baseDeDados.Nome = $"MB_{classesAleatorio[classe]}_{instanciasAleatorio[instancia]}";
+                    baseDeDados.Nome = $"MB_{classesAleatorio[classe]}_{instanciasAleatorioNome[instancia]}";
 
                     var ItensQuePrecisamSerRepetidos = baseDeDados.Itens.Where(x => x.Demanda > 1).ToList();
                     foreach (var itemParaRepetir in ItensQuePrecisamSerRepetidos)
